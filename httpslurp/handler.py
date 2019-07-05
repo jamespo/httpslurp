3b7fa02e19bb3f465ba80c261235b3dba0d59309
James Powell <jamespo@gmail.com>
2019-07-05 16:09:23 +0100

    cookies optional

--- httpslurp/handler.py
+++ httpslurp/handler.py
@@ -6,10 +6,17 @@ import uuid
 
 def requestdump(request, dumpdir):
     '''dump a request to disk'''
+    reqtime = str(time.time())
     filename = os.path.join(dumpdir,
-                            "%s-%s" % (time.time(), str(uuid.uuid4())))
+                            "%s-%s" % (reqtime, str(uuid.uuid4())))
     with open(filename, 'w') as logfile:
-        logfile.write(json.dumps({"parsed": True, "args": request.args,
-                                  "url": request.url,
-                                  "query_string": request.query_string}))
+        dumpdict = {"time": reqtime,
+                    "args": request.args,
+                    "headers": dict(request.headers),
+                    "url": request.url,
+                    "query_string": request.query_string}
+        if request.cookies != {}:
+            # add cookies if there are any
+            dumpdict["cookies"] = request.cookies
+        logfile.write(json.dumps(dumpdict))
     return filename
